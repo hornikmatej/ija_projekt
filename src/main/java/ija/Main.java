fb4d56f525951e1b7a0f967870f937565d87c17e
Matej Hornik <bachini256@gmail.com>
2021-04-05 22:03:46 +0200

    Upratany main

--- src/main/java/ija/Main.java
+++ src/main/java/ija/Main.java
@@ -24,7 +24,7 @@ import java.util.*;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primarystage) throws Exception{
+    public void start(Stage primarystage) throws Exception {
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/layout.fxml"));
         BorderPane root = loader.load();
@@ -34,62 +34,12 @@ public class Main extends Application {
 
         MainController controller = loader.getController();
         List<Drawable> elements = new ArrayList<>();
-        List<String> rady = new ArrayList<String>(){{
-            add("A");add("B");add("C");add("D");add("E");
-            add("F");add("G");add("H");add("I");
-        }};
-        Integer regal = 0;
 
-        int sirka_police = 20;
-        int dlzka_police = 20;
-
-        //vytvorenie polic
-
-            for (String rada : rady) {
-                if ( rada == "A") {
-                    for (int i = 2; i < 22; i = i + 2) {
-                        Shelf polica = new Shelf(rada + i, new Coordinate(115 + regal * 75, 55 + (i-2) / 2 * (sirka_police + 5)), sirka_police, dlzka_police);
-                        elements.add(polica);
-                        polica.setController(controller);
-                        polica.clickedOnShelf();
-                        polica.getGui().get(0).setFill(Color.WHITE);
-                        polica.getGui().get(0).setStroke(Color.BLACK);
-                    }
-                }
-                else if( rada == "I") {
-                    for (int i = 1; i < 20; i = i + 2) {
-                        Shelf polica = new Shelf(rada + i, new Coordinate(65 + regal * 75, 55 + (i) / 2 * (sirka_police + 5)), sirka_police, dlzka_police);
-                        elements.add(polica);
-                        polica.clickedOnShelf();
-                        polica.setController(controller);
-                        polica.getGui().get(0).setFill(Color.WHITE);
-                        polica.getGui().get(0).setStroke(Color.BLACK);
-                    }
-                }
-                else{
-                    for (int i = 2; i < 22; i = i + 2) {
-                        Shelf polica = new Shelf(rada + i, new Coordinate(115 + regal * 75, 55 + (i-2) / 2 * (sirka_police + 5)), sirka_police, dlzka_police);
-                        elements.add(polica);
-                        polica.clickedOnShelf();
-                        polica.setController(controller);
-                        polica.getGui().get(0).setFill(Color.WHITE);
-                        polica.getGui().get(0).setStroke(Color.BLACK);
-                    }
-                    for (int i = 1; i < 20; i = i + 2) {
-                        Shelf polica = new Shelf(rada + i, new Coordinate(65 + regal * 75, 55 + (i) / 2 * (sirka_police + 5)), sirka_police, dlzka_police);
-                        elements.add(polica);
-                        polica.clickedOnShelf();
-                        polica.setController(controller);
-                        polica.getGui().get(0).setFill(Color.WHITE);
-                        polica.getGui().get(0).setStroke(Color.BLACK);
-                    }
-                }
-            }
-            regal++;
-        }
+        Warehouse warehouse = new Warehouse(controller);
+        elements.addAll(warehouse.generateWarehouse());
 
         List<Coordinate> coordinates = new ArrayList<>();
-        coordinates.add(new Coordinate(40,40));
+        coordinates.add(new Coordinate(40, 40));
 
 //        List<Street> streets = new ArrayList<>();
 //        Street vyklad = new Street("VYKLAD/NAKLAD", new Coordinate(40 , 40), new Coordinate(100, 40));
@@ -99,10 +49,7 @@ public class Main extends Application {
 
         YAMLFactory factory = new YAMLFactory().disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER);
         ObjectMapper mapper = new ObjectMapper(factory);
-        Data data1 = mapper.readValue(new File("test.yml"),Data.class);
-
-        System.out.println(data1.getCoordinates());
-        System.out.println(data1.getVehicles());
+        Data data1 = mapper.readValue(new File("test.yml"), Data.class);
 
 
         elements.addAll(data1.getStreets());
--- src/main/java/ija/Warehouse.java
+++ src/main/java/ija/Warehouse.java
@@ -1,6 +1,7 @@
 package ija;
 
 import ija.store.Shelf;
+import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,11 +11,72 @@ public class Warehouse {
     private List<Shelf> shelves;
     private List<Street> streets;
     private List<Vehicle> vehicles;
+    private List<String> rady;
+    private int sirka_police = 20;
+    private int dlzka_police = 20;
+    private MainController controller = null;
 
-    public Warehouse() {
+    public Warehouse(MainController controller) {
+        this.controller = controller;
         this.shelves = new ArrayList<>();
         this.streets = new ArrayList<>();
         this.vehicles = new ArrayList<>();
+        rady = new ArrayList<String>(){{
+            add("A");
+            add("B");
+            add("C");
+            add("D");
+            add("E");
+            add("F");
+            add("G");
+            add("H");
+            add("I");
+        }};
+    }
+
+    public List<Drawable> generateWarehouse(){
+        List<Drawable> elements = new ArrayList<>();
+        int regal = 0;
+        for (String rada : rady) {
+            if (rada == "A") {
+                for (int i = 2; i < 22; i = i + 2) {
+                    Shelf polica = new Shelf(rada + i, new Coordinate(115 + regal * 75, 55 + (i - 2) / 2 * (sirka_police + 5)), sirka_police, dlzka_police);
+                    elements.add(polica);
+                    polica.setController(controller);
+                    polica.clickedOnShelf();
+                    polica.getGui().get(0).setFill(Color.WHITE);
+                    polica.getGui().get(0).setStroke(Color.BLACK);
+                }
+            } else if (rada == "I") {
+                for (int i = 1; i < 20; i = i + 2) {
+                    Shelf polica = new Shelf(rada + i, new Coordinate(65 + regal * 75, 55 + (i) / 2 * (sirka_police + 5)), sirka_police, dlzka_police);
+                    elements.add(polica);
+                    polica.clickedOnShelf();
+                    polica.setController(controller);
+                    polica.getGui().get(0).setFill(Color.WHITE);
+                    polica.getGui().get(0).setStroke(Color.BLACK);
+                }
+            } else {
+                for (int i = 2; i < 22; i = i + 2) {
+                    Shelf polica = new Shelf(rada + i, new Coordinate(115 + regal * 75, 55 + (i - 2) / 2 * (sirka_police + 5)), sirka_police, dlzka_police);
+                    elements.add(polica);
+                    polica.clickedOnShelf();
+                    polica.setController(controller);
+                    polica.getGui().get(0).setFill(Color.WHITE);
+                    polica.getGui().get(0).setStroke(Color.BLACK);
+                }
+                for (int i = 1; i < 20; i = i + 2) {
+                    Shelf polica = new Shelf(rada + i, new Coordinate(65 + regal * 75, 55 + (i) / 2 * (sirka_police + 5)), sirka_police, dlzka_police);
+                    elements.add(polica);
+                    polica.clickedOnShelf();
+                    polica.setController(controller);
+                    polica.getGui().get(0).setFill(Color.WHITE);
+                    polica.getGui().get(0).setStroke(Color.BLACK);
+                }
+            }
+            regal++;
+        }
+        return elements;
     }
 
     public void setShelves(List<Shelf> shelves) {
